4fc5d3680a5599655e789c55ca336e9df940e7f3
raj <rajkumer2001@gmail.com>
2022-12-27 21:07:48 +0600

    part-3

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::prefix('admin')->group(function(){
+    Route::get('dashboard',[DashboardController::class,'index']);
+});
