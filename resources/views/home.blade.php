12fdf56fcabc3938a1a9d0d5bb900b964dde2523
raj <rajkumer2001@gmail.com>
2022-12-27 23:59:53 +0600

    part-4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,8 +11,10 @@
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
+                            have
                         </div>
                     @endif
+
 
                     {{ __('You are logged in!') }}
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,34 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return view('welcome');
+    // return DB::select('SHOW TABLES');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::prefix('admin')->group(function(){
+
+Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function(){
     Route::get('dashboard',[DashboardController::class,'index']);
+});
+
+
+
+
+
+
+Route::get('demo',function(){
+   return $data =  User::find(19);
+    $data->role_as = 1;
+    $data->save();
+
+
 });
